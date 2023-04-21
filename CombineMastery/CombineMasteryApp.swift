2abe7bf0f644c7b40f6357727463f85ce372d620
Kraig Wastlund <kraigwastlund@gmail.com>
2023-04-21 14:52:58 -0600

    Initial Commit

---
+++ CombineMastery/CombineMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineMasteryApp.swift
+//  CombineMastery
+//
+//  Created by Kraig Wastlund on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
